893e15696cb178e30669c5e4515c23b007f74d82
unknown <ramyabandi9999@gmail.com>
2023-09-18 14:18:14 +0530

    first command

---
+++ src/main/java/com/example/demo/Dem2Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Dem2Application {
+	@GetMapping
+	public String helloWorld()
+	{
+		return "helloWorld";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dem2Application.class, args);
+	}
+
+}
